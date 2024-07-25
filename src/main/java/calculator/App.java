f9b0e5d5e96b848d924e2005eec3fcee3b77d53c
Robbie <robbie331@>
2024-07-25 20:25:55 +0900

    Level 1 - 3 입력받은 내용을 기반으로 사칙연산을 진행하며, 연산중 발생하는 오류 메시지를 정제하여 출력.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,15 +6,47 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int firstNumber;
+        int secondNumber;
+        char operator;
+        int result;
+
         System.out.print("첫 번째 숫자를 입력해주세요 : ");
-        int firstNumber = sc.nextInt();
+        firstNumber = sc.nextInt();
 
         System.out.print("두 번째 숫자를 입력해주세요 : ");
-        int secondNumber = sc.nextInt();
+        secondNumber = sc.nextInt();
 
         System.out.print("사칙연산 기호를 입력하세요 : ");
-        char operator = sc.next().charAt(0);
+        operator = sc.next().charAt(0);
 
-        System.out.println("입력받은 연산자 : " +operator);
+        try {
+            switch (operator) {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    System.out.println("두 수의 더하기의 결과는 : " + result);
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    System.out.println("두 수의 뺄셈의 결과는 : " + result);
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    System.out.println("두 수의 곱의 결과는 : " + result);
+                    break;
+                case '/':
+                    if(secondNumber == 0){
+                        throw new ArithmeticException("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                    }
+                    result = firstNumber / secondNumber;
+                    System.out.println("두 수의 나눗셈 결과는 : " + result);
+                    break;
+                default:
+                    System.out.println("잘못된 연산자의 입력입니다.");
+                    break;
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
